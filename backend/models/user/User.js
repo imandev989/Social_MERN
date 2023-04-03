983fe02aca3f250d0ae61749427e9da1e377ee8d
imandev <iman.dev989@gmail.com>
2023-04-03 19:16:19 +0330

    session09 - create user model

---
+++ backend/models/user/User.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    profilePhoto: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/3177/3177440.png",
+    },
+    email: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    accountVerificationToken: String,
+    accountVerificationTokenExpires: Data,
+    viewedBy: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          reference: "User",
+        },
+      ],
+    },
+    followers: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          reference: "User",
+        },
+      ],
+    },
+    following: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          reference: "User",
+        },
+      ],
+    },
+    passwordChangeAt: Data,
+    passwordResetToken: String,
+    passwordResetExpire: Date,
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
