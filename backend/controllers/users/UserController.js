ab03c210a08245e1fb507e125363b8b698d6a97b
imandev <iman.dev989@gmail.com>
2023-04-07 15:52:39 +0330

    modify register and intro middleware

--- backend/controllers/users/UserController.js
+++ backend/controllers/users/UserController.js
@@ -1,6 +1,10 @@
 import User from "../../models/user/User.js";
 
 export const userRegister = async (req, res) => {
+  const userExists = await User.findOne({ email: req.body.email });
+  if (userExists) {
+    return res.json("این کاربر قبلا ثبت نام کرده است");
+  }
   try {
     const user = await User.create({
       firstName: req?.body?.firstName,
