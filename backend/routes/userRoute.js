d860dd5d48c72e179594274207b931349d07ac48
imandev <iman.dev989@gmail.com>
2023-04-19 19:47:31 +0330

    create verify token

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { userLogin, userRegister, getUsers } from "../controllers/users/UserController.js";
+import { verifyToken } from "../middleware/token/VerifyToken.js";
 
 const router = express.Router();
 
 router.post("/api/users/register", userRegister);
 router.get("/api/users/login", userLogin);
-router.get("/api/users", getUsers);
+router.get("/api/users", verifyToken, getUsers);
 
 
 
